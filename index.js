b43d9bd896983ce801bf70eec9423c078a12038b
Odima-dev <vincentodima22@gmail.com>
2025-03-23 18:42:00 +0300

    setting up express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "Hello from the server!"})
+})
+
+app.listen(process.env.PORT)
