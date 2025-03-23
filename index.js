33645ec4d6b05063508059b28bcea9d652f13e66
Odima-dev <vincentodima22@gmail.com>
2025-03-23 18:57:17 +0300

    setting up express server

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors)
 app.use(helmet)
 app.use(cookieParser)
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.json({message: "Hello from the server!"})
 })
